d3c054f9872fc51e9fd27d99a13eae5bf5bf5c0c
Vlad <VladDaImpaler@gmail.com>
2020-10-06 04:20:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(inputText.value != ""){
+      createTask();
+      e.target.reset();
+    }else{ console.log("Task description cannot be blank.") }
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON"){
+      e.target.parentElement.remove()
+    }
+
+  });
 });
+
+const inputText = document.getElementById("new-task-description");
+const taskUl = document.getElementById("tasks");
+
+
+function createTask() {
+  let node = document.createElement("li");
+  let textnode = document.createTextNode(inputText.value);
+  let button = document.createElement("button");
+      button.innerHTML = "X";
+
+  node.appendChild(textnode);
+  node.appendChild(button);
+  taskUl.appendChild(node);
+}
